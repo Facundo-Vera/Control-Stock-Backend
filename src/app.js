d2617dad8a8f9ae962c245abe34656aa05002174
Facundo-Vera <facundovera.dev@gmail.com>
2026-05-18 12:46:32 -0300

    Agrego autenticacion

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import salesRoutes from "./routes/sales.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", salesRoutes);
+app.use("/api", authRoutes);
 
 export default app;
