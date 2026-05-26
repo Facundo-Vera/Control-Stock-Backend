6755f6932318a6ad43694227c2f729cb08aa2328
Facundo-Vera <facundovera.dev@gmail.com>
2026-05-26 16:04:00 -0300

    Configuracion de cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import salesRoutes from "./routes/sales.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
